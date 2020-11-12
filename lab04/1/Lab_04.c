147a7cba7a8a7144e2e222e0d4f77e8009062bd9
Dima <72880120+DimaBobchenok@users.noreply.github.com>
2020-11-12 16:18:20 +0200

    Update Lab_04.c

--- lab04/1/Lab_04.c
+++ lab04/1/Lab_04.c
@@ -1,15 +1,14 @@
-#define N 245 
-int main () {
+#define N 653
+int main () 
+{
 	int n1 = N/100;
 	int n2 = N%100/10;
 	int n3 = N%100%10;
-	
+	int result = 0
 	if (n1 == n2  || n1 == n3 || n3 == n2) {
-		
-			return 0;
-			
-		}
-		
-	return 1;
+	    result = 1 
+	}
+	return 0;
 }
+	
 	
