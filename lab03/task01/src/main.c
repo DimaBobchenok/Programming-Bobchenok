8edb0abc9fd2bae95b6009cdb57310c42aab679f
Dima <72880120+DimaBobchenok@users.noreply.github.com>
2020-11-19 12:52:55 +0200

    Add files via upload

---
+++ lab03/task01/src/main.c
@@ -0,0 +1,10 @@
+int main ()  {
+    #define  UAH 1000 // 1000 гривен (сумма)
+    const float eur=32; // 1 евро - 32 гривен 
+    const float usd=28; // 1 доллар - 28 гривен
+    const float rub=0.24; // 1 рубль  - 0.24 гривен 
+	float uah_euro = UAH / eur; //гривны в евро
+	float uah_usd = UAH / usd; //гривны в долларах
+	float uah_rub = UAH / rub; // гривны в рублях
+	return 0;
+}
