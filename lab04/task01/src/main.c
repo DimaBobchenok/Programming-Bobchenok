2ee87586b334b14dd5dda2c9db68a89959979302
Dima <72880120+DimaBobchenok@users.noreply.github.com>
2020-11-21 13:34:32 +0200

    Лабораторная №3
    Лабораторная №3

--- lab04/task01/src/main.c
+++ lab04/task01/src/main.c
@@ -1,12 +1,12 @@
 #define N 653
 int main () 
 {
-	int n1 = N/100; // Сотни
-	int n2 = N%100/10; // Десятки
-	int n3 = N%100%10; // Еденици
+	int n1 = N/100;
+	int n2 = N%100/10;
+	int n3 = N%100%10;
 	int result = 0;
-	if  (n1 == n2  || n1 == n3 || n3 == n2) { // Сравенине 
-	    result = 1; // Есть ли одинаковые числа
+	if  (n1 == n2  || n1 == n3 || n3 == n2) {
+	    result = 1;
 	}
 	return 0;
 
