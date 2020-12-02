ebff71e5ed666f7be06160ec11067bc7810cdaae
Dima <72880120+DimaBobchenok@users.noreply.github.com>
2020-12-02 15:51:38 +0200

    вихначити y(x) на заданих проміжках графіку

--- lab04/task05/scr/main.c
+++ lab04/task05/scr/main.c
@@ -1,1 +1,19 @@
 #define X 8 // значение х
+
+int main()
+{
+	  int y = 0;
+	  if (X < -1) {
+		         y = -1 / x;
+	  }
+	  else if (X > -1 && X <= 0) {
+		         y = X + 1;
+	  }
+	  else if (X > 0 && X <= 1) {
+		         y = -1 * X + 1;
+  	}
+	  else if (X > 1) {
+		         y = X - 1;
+	  }
+    return 0;
+}
